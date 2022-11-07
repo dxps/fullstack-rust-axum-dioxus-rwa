997f798ad5340a925fcbea0a1b6289940378c907
dxps <marius.ileana@gmail.com>
2022-11-07 23:51:38 +0200

    feat(be): add_article (wip, tags_articles table)

---
+++ backend/migrations/20221106211345_create_tags_tables.sql
@@ -0,0 +1,9 @@
+
+CREATE TABLE IF NOT EXISTS tags_articles (
+    tag                   VARCHAR(32),
+    article_id            BIGSERIAL,
+
+    PRIMARY KEY(tag, article_id),
+
+    CONSTRAINT fk_article_id   FOREIGN KEY(article_id)  REFERENCES articles(id),
+);
